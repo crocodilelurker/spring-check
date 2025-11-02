452bca757152801be78da1e44e3190e6b01c1b93
crocodilelurker <swagatsahu556@gmail.com>
2025-11-02 10:50:21 +0530

    Constructor Injection and Interface Based Coupling also some annotations

--- store/src/main/java/com/crocodilelurker/store/StoreApplication.java
+++ store/src/main/java/com/crocodilelurker/store/StoreApplication.java
@@ -2,11 +2,16 @@ package com.crocodilelurker.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StoreApplication.class, args);
+
+		var paymentService = new Paypal();
+		//SpringApplication.run(StoreApplication.class, args);
+		var orderService = new OrderService(paymentService);
+		orderService.placeOrder();
 	}
 }
