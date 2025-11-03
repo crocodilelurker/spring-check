267a1be03b60e5b7bdf22bb521d2a449a378b716
crocodilelurker <swagatsahu556@gmail.com>
2025-11-04 02:00:09 +0530

    Externalizing Configuration using YAML

--- store/src/main/java/com/crocodilelurker/store/Stripe.java
+++ store/src/main/java/com/crocodilelurker/store/Stripe.java
@@ -2,13 +2,17 @@ package com.crocodilelurker.store;
 
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service("stripe")
 public class Stripe implements PaymentService{
 
+    @Value("${stripe.apiUrl}")
+    private String apiUrl;
     @Override
     public void display() {
         System.out.println("Stripe Payment Initiated");
+        System.out.println("API URL: " + apiUrl);
     }
 }
